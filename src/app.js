1b48ad804181473218363eaceede57c020b9a916
nan-anomynous <nandinichaturvedi283@gmail.com>
2026-07-02 17:03:01 +0530

    first

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "http://primetrade-client-7r7l-coral.vercel.app/"
+      "https://primetrade-client-7r7l-coral.vercel.app/"
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
